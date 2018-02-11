c0fb62cd281769d4c31de7073dfeb17699a1e0aa
Shane Mc Cormack <shanemcc@gmail.com>
2018-02-11 01:20:11 +0000

    Change how JULHandler works to allow custom handlers.

--- src/uk/org/dataforce/libs/logger/Logger.java
+++ src/uk/org/dataforce/libs/logger/Logger.java
@@ -273,74 +273,22 @@ public class Logger {
     /**
      * Add a JUL handler.
      */
-    public static void addJULHandler() {
-        if (julHandler != null) { return; }
+    public static void setJULHandler() {
+        setJULHandler(new JULHandler(LogManager.getLogManager()));
+    }
 
-        julHandler = new JULHandler(LogManager.getLogManager());
+    /**
+     * Add a specific JUL handler.
+     *
+     * @param handler JULHandler to use.
+     */
+    public static void setJULHandler(final JULHandler handler) {
+        julHandler = handler;
+        julHandler.getManager().reset();
+        julHandler.getManager().getLogger("").addHandler(julHandler);
         julHandler.setLogLevel(getLevel());
     }
 
     /** Prevent instances of Logger */
     private Logger() {    }
-
-    /** java.util.logging Handler to redirect log messages. */
-    static class JULHandler extends Handler {
-        private final Map<Level, LogLevel> mappings = new HashMap<>();
-        private final Map<LogLevel, Level> reverseMappings = new HashMap<>();
-
-        private final LogManager manager;
-
-        private JULHandler(final LogManager manager) {
-            addMapping(Level.OFF, LogLevel.SILENT);
-            addMapping(Level.ALL, LogLevel.DEBUG9);
-            addMapping(Level.SEVERE, LogLevel.ERROR);
-            addMapping(Level.WARNING, LogLevel.WARNING);
-            addMapping(Level.INFO, LogLevel.INFO);
-            addMapping(Level.FINE, LogLevel.DEBUG);
-            reverseMappings.put(LogLevel.DEBUG2, Level.FINE);
-            addMapping(Level.FINER, LogLevel.DEBUG3);
-            reverseMappings.put(LogLevel.DEBUG4, Level.FINER);
-            addMapping(Level.FINEST, LogLevel.DEBUG5);
-            reverseMappings.put(LogLevel.DEBUG6, Level.FINEST);
-            reverseMappings.put(LogLevel.DEBUG7, Level.FINEST);
-            reverseMappings.put(LogLevel.DEBUG8, Level.FINEST);
-            reverseMappings.put(LogLevel.DEBUG9, Level.ALL);
-            addMapping(Level.CONFIG, LogLevel.INFO);
-
-            this.manager = manager;
-            manager.reset();
-            manager.getLogger("").addHandler(this);
-            super.setLevel(Level.ALL);
-        }
-
-        private void addMapping(final Level julLevel, final LogLevel ourLevel) {
-            mappings.put(julLevel, ourLevel);
-            reverseMappings.put(ourLevel, julLevel);
-        }
-
-        @Override
-        public void publish(final LogRecord record) {
-            final LogLevel level = mappings.getOrDefault(record.getLevel(), LogLevel.INFO);
-            final Object params[] = record.getParameters();
-            final String message = (params == null || params.length == 0) ? record.getMessage() : MessageFormat.format(record.getMessage(), record.getParameters());
-
-            Logger.log(level, message);
-        }
-
-        @Override
-        public void flush() { }
-
-        @Override
-        public void close() throws SecurityException { }
-
-        public void setLogLevel(final LogLevel level) {
-            final Level newLevel = reverseMappings.getOrDefault(level, Level.INFO);
-            manager.getLogger("").setLevel(newLevel);
-        }
-
-        @Override
-        public void setLevel(final Level level) {
-            throw new UnsupportedOperationException("This handler does not allow setting the log level.");
-        }
-    }
 }
